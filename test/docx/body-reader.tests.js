dc23584f6768634967d7739ff6029774719eab7c
Tobias Bocanegra <tripod@adobe.com>
2020-06-18 11:30:38 +0900

    Feat: add support for image title

--- test/docx/body-reader.tests.js
+++ test/docx/body-reader.tests.js
@@ -877,13 +877,15 @@ test("when v:imagedata element has no relationship ID then it is ignored with wa
 test("can read inline pictures", function() {
     var drawing = createInlineImage({
         blip: createEmbeddedBlip(IMAGE_RELATIONSHIP_ID),
-        description: "It's a hat"
+        description: "It's a hat",
+        title: "Sombrero"
     });
 
     var result = readEmbeddedImage(drawing);
 
     return promiseThat(result, isSuccess(contains(isImage({
         altText: "It's a hat",
+        title: "Sombrero",
         contentType: "image/png",
         buffer: IMAGE_BUFFER
     }))));
@@ -898,6 +900,7 @@ test("alt text title is used if alt text description is missing", function() {
     var result = readEmbeddedImage(drawing);
 
     return promiseThat(result, isSuccess(contains(isImage({
+        title: "It's a hat",
         altText: "It's a hat"
     }))));
 });
@@ -912,7 +915,8 @@ test("alt text title is used if alt text description is blank", function() {
     var result = readEmbeddedImage(drawing);
 
     return promiseThat(result, isSuccess(contains(isImage({
-        altText: "It's a hat"
+        altText: "It's a hat",
+        title: "It's a hat"
     }))));
 });
 
@@ -926,7 +930,8 @@ test("alt text description is preferred to alt text title", function() {
     var result = readEmbeddedImage(drawing);
 
     return promiseThat(result, isSuccess(contains(isImage({
-        altText: "It's a hat"
+        altText: "It's a hat",
+        title: "hat"
     }))));
 });
 
